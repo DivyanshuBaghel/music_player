0a18d7daace7578d1a6f1c083ee57637f56035c3
Divyanshu Baghel <divyanshubaghel1@gmail.com>
2023-02-11 11:00:30 +0530

    Day 4 fix build try 1

--- oldComponents.js/MusicList.js
+++ oldComponents.js/MusicList.js
@@ -1,60 +1,60 @@
-import { Text, View , ScrollView ,StyleSheet,Button} from 'react-native'
-import React, { Component } from 'react'
-import * as MediaLibrary from 'expo-media-library';
-import MusicPlayer from './MusicPlayer';
-import MusicPlayerV2 from './MusicPlayerV2';
-// import MusicPlayerV3 from './MusicPlayerV3';
+// import { Text, View , ScrollView ,StyleSheet,Button} from 'react-native'
+// import React, { Component } from 'react'
+// import * as MediaLibrary from 'expo-media-library';
+// import MusicPlayer from './MusicPlayer';
+// import MusicPlayerV2 from './MusicPlayerV2';
+// // import MusicPlayerV3 from './MusicPlayerV3';
 
 
 
 
-export default class MusicList extends Component {
-    state = {
-        files: [],
-      };
+// export default class MusicList extends Component {
+//     state = {
+//         files: [],
+//       };
       
     
-    async componentDidMount() {
-        const { status } = await MediaLibrary.requestPermissionsAsync();
-        if (status === 'granted') {
-          const assets = await MediaLibrary.getAssetsAsync({
-            first: 1000,
-            mediaType:"audio",
-            mimeTypes: ['audio/mpeg'],
-          });
-          alert(JSON.stringify(assets))
-          this.setState({ files: assets.assets });
-        }
-    }
+//     async componentDidMount() {
+//         const { status } = await MediaLibrary.requestPermissionsAsync();
+//         if (status === 'granted') {
+//           const assets = await MediaLibrary.getAssetsAsync({
+//             first: 1000,
+//             mediaType:"audio",
+//             mimeTypes: ['audio/mpeg'],
+//           });
+//           alert(JSON.stringify(assets))
+//           this.setState({ files: assets.assets });
+//         }
+//     }
 
-  render() {
-    return (
-      <View style={styles.container}>
-      <ScrollView style={styles.list}>
-        {this.state.files.map((file) => (
-          <React.Fragment key={file.id}>
-            <MusicPlayer uri={file.uri} name={file.filename}/>
-          </React.Fragment>
-        ))}
-      </ScrollView>
-      <View style={styles.footer}>
-          <MusicPlayerV2/>
-      </View>
-      {/* <MusicPlayerV3 files={this.state.files.toString()}/> */}
-      </View>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//       <ScrollView style={styles.list}>
+//         {this.state.files.map((file) => (
+//           <React.Fragment key={file.id}>
+//             <MusicPlayer uri={file.uri} name={file.filename}/>
+//           </React.Fragment>
+//         ))}
+//       </ScrollView>
+//       <View style={styles.footer}>
+//           <MusicPlayerV2/>
+//       </View>
+//       {/* <MusicPlayerV3 files={this.state.files.toString()}/> */}
+//       </View>
+//     )
+//   }
+// }
 
-const styles = StyleSheet.create({
-    container:{
-      flex:1,
-    },
-    list:{
+// const styles = StyleSheet.create({
+//     container:{
+//       flex:1,
+//     },
+//     list:{
       
-    },
-    footer:{
-      justifyContent: "flex-end",
-      marginBottom:30,
-    }
-})
+//     },
+//     footer:{
+//       justifyContent: "flex-end",
+//       marginBottom:30,
+//     }
+// })
--- oldComponents.js/MusicPlayerV2.js
+++ oldComponents.js/MusicPlayerV2.js
@@ -1,104 +1,104 @@
-import React, { Component } from 'react';
-import { View, Button, StyleSheet } from 'react-native';
-import {Audio} from 'expo-av';
-import * as MediaLibrary from 'expo-media-library';
+// import React, { Component } from 'react';
+// import { View, Button, StyleSheet } from 'react-native';
+// import {Audio} from 'expo-av';
+// import * as MediaLibrary from 'expo-media-library';
 
 
 
 
-export default class MusicPlayerV2 extends Component {
-  state = {
-    soundFiles: [],
-    isPlaying: false,
-    currentSound: 0,
-    soundObject: null,
-  };
+// export default class MusicPlayerV2 extends Component {
+//   state = {
+//     soundFiles: [],
+//     isPlaying: false,
+//     currentSound: 0,
+//     soundObject: null,
+//   };
 
   
 
-  async componentDidMount() {
-      const { status } = await MediaLibrary.requestPermissionsAsync();
-      if (status === 'granted') {
-        const assets = await MediaLibrary.getAssetsAsync({
-          first: 1000,
-          mediaType:"audio",
-          mimeTypes: ['audio/mpeg'],
-        });
-        // alert(JSON.stringify(assets))
-        this.setState({ soundFiles: assets.assets });
-      }
-      const soundObject = new Audio.Sound();
-      await soundObject.loadAsync(this.state.soundFiles[this.state.currentSound]);
-      this.setState({ soundObject });
-  }
+//   async componentDidMount() {
+//       const { status } = await MediaLibrary.requestPermissionsAsync();
+//       if (status === 'granted') {
+//         const assets = await MediaLibrary.getAssetsAsync({
+//           first: 1000,
+//           mediaType:"audio",
+//           mimeTypes: ['audio/mpeg'],
+//         });
+//         // alert(JSON.stringify(assets))
+//         this.setState({ soundFiles: assets.assets });
+//       }
+//       const soundObject = new Audio.Sound();
+//       await soundObject.loadAsync(this.state.soundFiles[this.state.currentSound]);
+//       this.setState({ soundObject });
+//   }
   
 
-   playSound = async () => {
-    try {
-      const soundObject = new Audio.Sound();
-      await soundObject.loadAsync(this.state.soundFiles[this.state.currentSound]);
-      this.setState({ soundObject });
-      await soundObject.playAsync();
-      this.setState({ isPlaying: true });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//    playSound = async () => {
+//     try {
+//       const soundObject = new Audio.Sound();
+//       await soundObject.loadAsync(this.state.soundFiles[this.state.currentSound]);
+//       this.setState({ soundObject });
+//       await soundObject.playAsync();
+//       this.setState({ isPlaying: true });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
-  stopSound = async () => {
-    try {
-      await this.state.soundObject.stopAsync();
-      this.setState({ isPlaying: false });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//   stopSound = async () => {
+//     try {
+//       await this.state.soundObject.stopAsync();
+//       this.setState({ isPlaying: false });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
-  playNextSound = async () => {
-    try {
-      await this.state.soundObject.stopAsync();
-      this.setState(prevState => ({
-        currentSound: (prevState.currentSound + 1) % this.state.soundFiles.length,
-        soundObject: null
-      }));
-      this.playSound();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//   playNextSound = async () => {
+//     try {
+//       await this.state.soundObject.stopAsync();
+//       this.setState(prevState => ({
+//         currentSound: (prevState.currentSound + 1) % this.state.soundFiles.length,
+//         soundObject: null
+//       }));
+//       this.playSound();
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
-  playPreviousSound = async () => {
-    try {
-      await this.state.soundObject.stopAsync();
-      this.setState(prevState => ({
-        currentSound: (prevState.currentSound - 1 + this.state.soundFiles.length) % this.state.soundFiles.length,
-        soundObject: null
-      }));
-      this.playSound();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//   playPreviousSound = async () => {
+//     try {
+//       await this.state.soundObject.stopAsync();
+//       this.setState(prevState => ({
+//         currentSound: (prevState.currentSound - 1 + this.state.soundFiles.length) % this.state.soundFiles.length,
+//         soundObject: null
+//       }));
+//       this.playSound();
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Button title="Previous" onPress={this.playPreviousSound} />
-        {this.state.isPlaying ? (
-          <Button title="Stop" onPress={this.stopSound} />
-        ) : (
-          <Button title="Play" onPress={this.playSound} />
-        )}
-        <Button title="Next" onPress={this.playNextSound} />
-      </View>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Button title="Previous" onPress={this.playPreviousSound} />
+//         {this.state.isPlaying ? (
+//           <Button title="Stop" onPress={this.stopSound} />
+//         ) : (
+//           <Button title="Play" onPress={this.playSound} />
+//         )}
+//         <Button title="Next" onPress={this.playNextSound} />
+//       </View>
+//     );
+//   }
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row'
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     flexDirection: 'row'
+//   },
+// });
